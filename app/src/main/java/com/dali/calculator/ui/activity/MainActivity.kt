06a55e0add8f36b62ddf3e91923242f50f245ea0
zhuangqing <zhuangqing@xiaomi.com>
2022-08-24 18:56:31 +0800

    [init]初始化

---
+++ app/src/main/java/com/dali/calculator/ui/activity/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.dali.calculator.ui.activity
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.dali.calculator.ui.screen.CalculatorScreen
+import com.dali.calculator.ui.theme.CalculatorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CalculatorTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    CalculatorScreen()
+                }
+            }
+        }
+    }
+}
+
+
+
---
+++ app/src/main/java/com/dali/calculator/ui/screen/CalculatorScreen.kt
@@ -0,0 +1,21 @@
+package com.dali.calculator.ui.screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CalculatorScreen(){
+
+    Column(Modifier.fillMaxSize()) {
+
+
+
+
+    }
+
+
+
+
+}
